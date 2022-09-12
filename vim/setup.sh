02d1e9cd95e2ca142b32e6ebd8fab71725996f75
QingGo <zyqingjohn@qq.com>
2022-09-12 16:44:50 +0800

    set up golang dev env

--- vim/setup.sh
+++ vim/setup.sh
@@ -4,3 +4,8 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SCRIPT_DIR" || 
 
 ln -sf "$SCRIPT_DIR/vimrc" ~/.vimrc
 ln -snf "$SCRIPT_DIR/nvim" ~/.config/nvim
+
+# install vim-plug which is a plug-in manager
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+nvim --headless +PlugInstall +qall
